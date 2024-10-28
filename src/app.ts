4d0a215b3a31116f43428e34ae16f3d7afd081e4
Schwartz <darlanswtz@gmail.com>
2024-10-28 05:47:02 -0300

    Feat: added error handling on all routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import { mainRouter } from "./routes/routes";
 import "express-async-errors";
+import errorHandler from "./middlewares/error.middleware";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(json());
 app.use(cors());
 app.use(mainRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,60 +1,21 @@
 
-// import { NextFunction, Request, Response } from "express";
-// import { isAxiosError } from "axios";
-// enum HttpStatus {
-//     OK = 200,
-//     CREATED = 201,
-//     CONFLICT = 409,
-//     NOT_FOUND = 404,
-//     UNPROCESSABLE_ENTITY = 422,
-//     BAD_REQUEST = 400,
-//     INTERNAL_SERVER_ERROR = 500,
-//     UNAUTHORIZED = 401,
-//     FORBIDDEN = 403,
-//     NO_CONTENT = 204,
-//     PAYMENT_REQUIRED = 402,
-//     ACCEPTED = 202,
-// }
+import { NextFunction, Request, Response } from "express";
+enum HttpStatus {
+    OK = 200,
+    CREATED = 201,
+    CONFLICT = 409,
+    NOT_FOUND = 404,
+    UNPROCESSABLE_ENTITY = 422,
+    BAD_REQUEST = 400,
+    INTERNAL_SERVER_ERROR = 500,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NO_CONTENT = 204,
+    PAYMENT_REQUIRED = 402,
+    ACCEPTED = 202,
+}
 
-// export default function errorHandler(error: unknown, req: Request, res: Response, _next: NextFunction) {
-
-//     if (isAxiosError(error)) return res.status(error?.status ?? HttpStatus.INTERNAL_SERVER_ERROR).send(error?.response?.data);
-//     const ErrorBody = createErrorBody(error);
-//     if (error && typeof error === "object" && "type" in error) {
-//         switch (error.type) {
-//             case "Conflict":
-//                 return res.status(HttpStatus.CONFLICT).send(ErrorBody);
-//             case "NotFound":
-//                 return res.status(HttpStatus.NOT_FOUND).send(ErrorBody);
-//             case "Unprocessable":
-//                 return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send(ErrorBody);
-//             case "Validation":
-//                 return res.status(HttpStatus.BAD_REQUEST).send(ErrorBody);
-//             case "Unauthorized":
-//                 return res.status(HttpStatus.UNAUTHORIZED).send(ErrorBody);
-//             case "Processing":
-//                 return res.status(HttpStatus.NO_CONTENT).send(ErrorBody);
-//             case "Internal":
-//                 return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorBody);
-//             case "Forbidden":
-//                 return res.status(HttpStatus.FORBIDDEN).send(ErrorBody);
-//             case "Critical":
-//                 return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorBody);
-//             case "BadRequest":
-//                 return res.status(HttpStatus.BAD_REQUEST).send(ErrorBody);
-//             case "PaymentRequired":
-//                 return res.status(HttpStatus.PAYMENT_REQUIRED).send(ErrorBody);
-//             case "Error":
-//                 return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorBody);
-//             default:
-//                 return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorBody);
-//         }
-//     }
-
-//     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorBody);
-// }
-
-// function createErrorBody(error: unknown) {
-//     if (typeof error === "string") return { message: error };
-//     return error;
-// }
+export default function errorHandler(error: unknown, req: Request, res: Response, _next: NextFunction) {
+    console.error(error);
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+}
